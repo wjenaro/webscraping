0f41da986de2f42b59cad73ee7fa3edf7959009a
Jenaro Wachira <wjenaro@gmail.com>
2023-06-19 15:07:25 +0300

    downloaded a image of a monkey

--- grab_images.py
+++ grab_images.py
@@ -6,3 +6,13 @@ soup=bs4.BeautifulSoup(results.text, "lxml")
 
 
 print(soup.select('.image-section')[0])
+print("======determine the stirng to download=======================")
+getSrc=soup.select('.image-section')[0]
+print(getSrc)
+print("Getting image from website +++++++++++++++++++++++++++++++++++")
+imag=requests.get("https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg/220px-Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg")
+
+f=open('monkey.jpg','wb')
+f.write(imag.content)
+f.close
+print("Saved+++++++++++++++++++++++++++++")
